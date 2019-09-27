993805f0791fc4ea9df849e324c055f334fe2877
vini2reis <vinicius.reis@estudante.uffs.edu.br>
2019-09-27 14:10:53 -0300

    atualiza exercicio 1

--- ex1.asm
+++ ex1.asm
@@ -6,20 +6,25 @@ msg:	.string "Informe o valor: "
 	.text
 
 main:
-	la	a0, vetor
 	li 	a1, 3
-	add	a2, zero, a0
+	add	a2, zero, zero
 	jal	le_vetor
 	nop
 	ebreak
 	
 le_vetor:
+	la	a0, vetor
 	beq	a1, zero, fim
+	add	a3, a0, zero
 	la	a0, msg		
 	li	a7, 4		
 	ecall
 	li	a7, 5
 	ecall
+	add	s0, a0, zero
+	add	a0, a3, zero
+	add	a0, a0, a2
+	sw	s0, (a0)
 	addi	a2, a2, 4	
 	addi 	a1, a1, -1 	
 	j	le_vetor
